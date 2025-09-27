9e2bd14c6f32bc86fb387774bb9420d4bed6884b
MuddanaSindhuja <sindhu.muddana@gmail.com>
2025-09-27 12:01:50 +0530

    3rd commit

--- mongodb.sh
+++ mongodb.sh
@@ -47,3 +47,10 @@ VALIDATE $? "Enable MongoDB"
 
 systemctl start mongod
 VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to mongodb"
+
+
+systemctl restart mongod
+VALIDATE $? "Restarted mongodb"
